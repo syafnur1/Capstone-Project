4b3d7a1eef045d9c9e6fe3081cfac9c199797ff8
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-17 23:48:40 +0700

    Update Capstone_Project.py

--- Capstone_Project.py
+++ Capstone_Project.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-import ee
-import geemap.foliumap as geemap
 
 st.set_page_config(layout="wide")
 image = Image.open("Barantum.png")
