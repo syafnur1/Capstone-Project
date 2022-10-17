e0a3b62141d134479f675657c8c514c22fafb36f
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-17 17:57:49 +0700

    Update Capstone_Project.py

--- Capstone_Project.py
+++ Capstone_Project.py
@@ -32,11 +32,13 @@ st.subheader("Hubungan Jumlah Turis dengan Pandemi")
 col1, col2 = st.columns(2)
 with col1:
    st.subheader("Jumlah Turis per Pintu Masuk")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
+    image1 = Image.open("Pinturis.png")
+   st.image(image1, use_column_width='auto', caption ='Sumber : Badan Pusat Statistik')
 
 with col2:
    st.subheader("Kasus Positif Harian Covid di Indoneai")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
+    image2 = Image.open("Positif.png")
+   st.image(image2, use_column_width='auto', caption ='Sumber : kawalcovid19.id')
 
 # Grafik korelasi
 ''' Dari kedua grafik diatas dapat diambil kesimpulan bahwa kasus Covid-19 yang muncul di tahun 2020 mempengaruhi jumlah wisman 
