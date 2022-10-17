44ecb783349d0b3fa2740212a854fb2417c263f1
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-17 17:36:32 +0700

    Update Capstone_Project.py

--- Capstone_Project.py
+++ Capstone_Project.py
@@ -19,13 +19,28 @@ st.image(image, use_column_width='auto', caption = "Sumber : barantum.com")
 [Pariwisata](https://id.wikipedia.org/wiki/Pariwisata) merupakan cara suatu negara mempromosikan negara tersebut sehingga dapat dikinjungi oleh banyak orang. Di Indonesia, contoh pariwisata yang menjadi tujuan wisata yang terkenal di dunia adalah [Bali](https://bali.com/). Bali terkenal dengan berbagai objek wisatanya seperti wisata alam, wisata budaya, dan wisata bahari
 '''
 
-# Deklarasi dataset
 st.subheader("Jumlah Wisatawan Asing Masuk ke Indonesia")
 df = pd.DataFrame([[13514963], [16106954], [4052923], [1557530], [202823]],
                     index=['2018', '2019', '2020', '2021', '2022'],
                     columns=['Jumlah Turis'])
 st.line_chart(data=df, use_container_width=True) 
 st.caption('Sumber : Badan Pusat Statistik')
+
+col1, col2 = st.columns(2)
+
+st.subheader("Hubungan Jumlah Turis dengan Pandemi")
+
+with col1:
+   st.header("Jumlah Turis per Pintu Masuk")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("Kasus Positif Harian Covid di Indoneai")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+
+# Deklarasi dataset
+#df1 = pd.read_excel('/content/Data Gabungan Turis.xlsx', sheet_name='Sheet2')
 
 # Data prep
 
@@ -64,4 +79,6 @@ mayoritas berasal dari benua Asia dan Australia yang jaraknya tidak jauh dari In
 [Kawal Covid 19](https://docs.google.com/spreadsheets/d/1ma1T9hWbec1pXlwZ89WakRk-OfVUQZsOCFl4FwZxzVw/htmlview)
 
 [Model Decision Tree untuk Prediksi Jadwal Kerja menggunakan Scikit-Learn](https://jurnal.umj.ac.id/index.php/semnastek/article/view/5239/3517)
+
+[barantum.com](https://www.barantum.com/blog/wp-content/uploads/2019/01/Wisata-Indonesia-Mempunyai-Peluang-Bisnis.jpg)
 '''   
