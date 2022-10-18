d23c30ef7c38e97a8f21fc306f14b8a16e1100f8
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-19 00:50:23 +0700

    Update Capstone_Project.py

--- Capstone_Project.py
+++ Capstone_Project.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plost
 import plotly.express as px
+import plotly.graph_objects as go
 from PIL import Image
 
 st.set_page_config(page_title="Capstone Project",
