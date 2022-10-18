a6d4abdb31c8bbf0173d2ce2cd127d2be00429f7
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-19 00:01:58 +0700

    Update Capstone_Project.py

--- Capstone_Project.py
+++ Capstone_Project.py
@@ -98,31 +98,7 @@ with negara:
     country = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTXspAWpKN-lhLVzwafiDNnwXAUf_l_I-xsdO3AVT0bDzTsgS5NyMnaOQRB865eBscEt9NKka4cJ-pw/pub?gid=0&single=true&output=csv')
     country = country.drop(['Grand Total'], axis=1)
     
-    st.dataframe(country, use_container_width=True)
-    
-    top2018 = st.multiselect("Pilihan :", 
-                          options=country["2018"].unique(),
-                          default=country["2018"].unique()
-                         )
-    top2019 = st.multiselect("Pilihan :", 
-                          options=country["2019"].unique(),
-                          default=country["2019"].unique()
-                         )
-    top2020 = st.multiselect("Pilihan :", 
-                          options=country["2020"].unique(),
-                          default=country["2020"].unique()
-                         )
-    top2021 = st.multiselect("Pilihan :", 
-                          options=country["2021"].unique(),
-                          default=country["2021"].unique()
-                         )
-    top2022 = st.multiselect("Pilihan :", 
-                          options=country["2022"].unique(),
-                          default=country["2022"].unique()
-                         )
-    country_selection = country.query(
-      "2018 == @top2018 & 2019 == @top2019 & 2020 == @top2020 & 2021 == @top2021 & 2022 == @top2022"
-    )
+    st.dataframe(country.style.highlight_max(axis=0), use_container_width=True)
     
     st.markdown('''Dapat dilihat bahwa **Top 5** Turis yang banyak berkunjung ke Indonesia adalah negara-negara tetangga, dan mayoritas negara 
                 tersebut berasal dari benua Asia yang jaraknya tidak jauh dari Indonesia.''')
