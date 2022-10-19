9d88f642dfb24e59d8cf57c4ae2e2b2b2b41fdac
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-19 14:01:27 +0700

    Update Capstone_Project.py

--- Capstone_Project.py
+++ Capstone_Project.py
@@ -185,10 +185,11 @@ groupby_column = st.selectbox(
 # -- Group Negara
 output_columns =['2018',  '2019', '2020', '2021',  '2022']
 country_grouped = country.sort_values(by = [groupby_column], ascending=False,)[output_columns].head(5)
- 
+negara = country["Negara"]
+
 gra_country = px.bar(country_grouped,
                      x=groupby_column,
-                    
+                     y=negara.index,
                      title='<b>Jumlah Turis berdasarkan Negara<b>',
                      template="plotly_white",
                     )
