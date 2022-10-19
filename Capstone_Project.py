a0e80e2c3ef06810fe4ce51ef7397a8214259182
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-19 14:28:27 +0700

    Update Capstone_Project.py

--- Capstone_Project.py
+++ Capstone_Project.py
@@ -124,7 +124,7 @@ with negara:
     
     gra_country.update_layout(
       plot_bgcolor="white",
-      xaxis=dict(tickmode="linear",
+      xaxis=dict(
       showgrid=False, 
       showline=False,
       showticklabels=True,
